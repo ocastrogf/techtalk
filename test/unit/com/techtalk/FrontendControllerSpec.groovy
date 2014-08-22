089c1587e527516f5cd6d019af7d374795f6b1f9
Oliver Castro <olivertamboboycastro@gmail.com>
2014-08-21 22:58:09 -0700

    Initialize

---
+++ test/unit/com/techtalk/FrontendControllerSpec.groovy
@@ -0,0 +1,20 @@
+package com.techtalk
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(FrontendController)
+class FrontendControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
